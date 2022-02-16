0b12e7b9a36b3e0a3c09131f7894f3ab5a29aa10
Mawuli Kwadzofio <princekd131@gmail.com>
2022-02-16 16:47:28 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
             AnimOne(),
             DateWidget(),
             TimeWidget(),
+            TileTwo(),
           ],
         )));
   }
